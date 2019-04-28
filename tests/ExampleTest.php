b3a6fb3d981575f0c4c93ebb3620628651384b17
Neumann <neumann@tonda.info>
2019-04-28 19:35:18 +0200

    Add one example test instead of .keep file

---
+++ tests/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace Morgo;
+
+use PHPUnit\Framework\TestCase;
+
+final class ExampleTest extends TestCase
+{
+    public function testExample() {
+        $this->assertTrue(true);
+    }
+}
